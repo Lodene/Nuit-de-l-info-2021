5ed1f2d4a728061b153725b3a6205f2197a7c4b0
Sidem <antoine.adjamidis@gmail.com>
2021-12-03 04:57:06 +0100

    ajout d'un header et commencement du storyManager.php

--- backend/controler/getResult.php
+++ backend/controler/getResult.php
@@ -1,6 +1,7 @@
 <?php
     require('../model/rescuerManager.php');
     header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *');
     
 
     $send['success'] = false;
